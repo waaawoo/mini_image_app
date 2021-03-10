8fcd0ab315052e1bd661a1e7b1dc93c0d637a621
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-03-10 12:08:55 +0900

    処理が不要な画面ではjsを実行しない

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,24 +1,27 @@
-document.addEventListener("DOMContentLoaded", function(){
-  // 画像表示領域の要素を取得
-  const ImageList = document.getElementById("image-list");
-  // ファイルが選択されていませんの箇所に変化が起きたらイベント発火
-  document.getElementById("message_image").addEventListener("change", function(e){
-    // 画像情報がEventハッシュに格納されているので変数へ格納する
-    const file = e.target.files[0];
-    // 引数に渡した画像のURLを作成
-    const blob = window.URL.createObjectURL(file);
+// URLにnew editがない場合は実行されない処理を追加
+if (document.URL.match( /new/ ) || document.URL.match( /edit/ ) ){
+  document.addEventListener("DOMContentLoaded", function(){
+    // 画像表示領域の要素を取得
+    const ImageList = document.getElementById("image-list");
+    // ファイルが選択されていませんの箇所に変化が起きたらイベント発火
+    document.getElementById("message_image").addEventListener("change", function(e){
+      // 画像情報がEventハッシュに格納されているので変数へ格納する
+      const file = e.target.files[0];
+      // 引数に渡した画像のURLを作成
+      const blob = window.URL.createObjectURL(file);
 
-    // jsでhtmlを生成する
-    // divタグを生成
-    const imageElement = document.createElement("div");
-    // imgタグを生成
-    const blobImage = document.createElement("img");
-    // 上で生成したimgタグのsrc属性に作成したURLを指定
-    blobImage.setAttribute("src", blob);
-    // 作成したdivタグの中にimgタグを追加する
-    imageElement.appendChild(blobImage);
-    // 既存のHTMLへ作成したdivタグを挿入
-    ImageList.appendChild(imageElement);
+      // jsでhtmlを生成する
+      // divタグを生成
+      const imageElement = document.createElement("div");
+      // imgタグを生成
+      const blobImage = document.createElement("img");
+      // 上で生成したimgタグのsrc属性に作成したURLを指定
+      blobImage.setAttribute("src", blob);
+      // 作成したdivタグの中にimgタグを追加する
+      imageElement.appendChild(blobImage);
+      // 既存のHTMLへ作成したdivタグを挿入
+      ImageList.appendChild(imageElement);
 
+    });
   });
-});
+}
